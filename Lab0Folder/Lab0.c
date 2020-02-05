6167bf56e9d11733e22d1d9be6d16a2238afcd57
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-02-05 15:07:56 -0800

    Create Lab0.c
    Second attempt to get the code in git.

---
+++ Lab0Folder/Lab0.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+        int main(){
+                printf("Original   ASCII   Decimal   Parity   T. Error  \n -------- -------- -------- -------- --------");
+        }
+~           
