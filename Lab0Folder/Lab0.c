c8262c20a3c0809e586c65f99875c734cb1769ee
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-02-05 16:19:05 -0800

    Update Lab0
    Writing some file read stuff.

--- Lab0Folder/Lab0.c
+++ Lab0Folder/Lab0.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
         int main(){
+                char num;
+                char name_file;
+                File *fl;
+                gets(name_file);
+                fl = fopen(name_file,"r");
+                if(fl == NULL){
+                        perror("Ooopsie file not read\n");
+                        exit(EXIT_FAILURE);
+                }
                 printf("Original   ASCII   Decimal   Parity   T. Error  \n -------- -------- -------- -------- --------");
         }
 ~           
