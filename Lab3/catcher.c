fe097baa344d66cc9034aad163f0ed9ecc23c7cd
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-05-06 22:09:57 -0700

    Update catcher.c

--- Lab3/catcher.c
+++ Lab3/catcher.c
@@ -1,1 +1,41 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h> 
 
+static int count = 0;
+static char* SIGS[27] = {"HUP", "INT", "QUIT", "ILL", "TRAP",
+                         "ABRT", "BUS", "FPE", "KILL", "USR1",
+                         "SEGV", "USR2", "PIPE", "ALRM", "TERM", 
+                         "STKFLT", "CHLD", "CONT", "STOP", "TSTP",
+                         "TTIN", "TTOUT", "URG", "XCPU", "XFSZ",
+                         "VTALRM", "PROF", "WINCH"};
+
+void sigHandler(int sig){
+  
+  
+  
+}
+
+
+void sigCatch(int sig){
+  
+  
+  
+}
+
+int main(int argc, char **argv){
+pid_t pid;
+
+time_t sec;
+time(&sec);
+
+  
+  retrun 0;
+  
+
+}
