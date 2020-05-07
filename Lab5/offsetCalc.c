4e55e8bc1e5ee740cb140953a2591abe8c3ac1d0
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-05-06 21:01:44 -0700

    Update offsetCalc.c
    Ez lab

--- Lab5/offsetCalc.c
+++ Lab5/offsetCalc.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void printA(int num){
+    printf("The address %d contains:\n", num);
+ 
+}
+
+void printPN(int num){
+  printf("Page Number = %d\n", num);
+  
+}
+
+void printO(int num){
+  printf("Offset = %d\n", num);
+  
+}
+
+int main(int argc, char **argv){
+  //define local variables
+  //4kb = 4096 bytes
+  const int size = 4096;
+  unsigned int pageNum, address, offset;
+  
+  //check if # is < than 2
+  if(argc < 2){
+   printf("Error: not enough number of arguments");
+    
+    return EXIT_FAILURE;
+  }//end if
+  
+  //Convert value into integer
+  address = atoi(argv[1]);
+  //computation of page number and offset
+  pageNum = address/size;
+  offset = address % size;
+  
+  //Display address, page number, and offset
+printA(address);
+printPN(pageNum);
+printO(offset);
+  
+  return 0;
+  
+}//end main
