3a45787d02ab09ccba01fe22621f8ca8916a364f
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-05-07 01:34:58 -0700

    Update catcher.c

--- Lab3/catcher.c
+++ Lab3/catcher.c
@@ -27,13 +27,34 @@ void sigCatch(int sig){
   else{
         term = 0;
     }
-  
+if(n == SIGUSR1) {
+        fprintf(stdout, "SIGUSR1 caught at %ld\n", sec);
+	    
+    } else if(n == SIGUSR2) {
+        fprintf(stdout, "SIGUSR2 caught at %ld\n", sec);
+	    
+    }else {
+        
+        fprintf(stdout, "SIG%s caught at %ld\n", SIGS[n-1], sec);
+    }
+    count++;
   
 }
 
 int main(int argc, char **argv){
 pid_t pid;
-
+for(int i = 1; i < argc; i++) {
+        for(int j = 0; j < 27; j++) {
+  
+          if(strcmp(argv[i], "USR1")){
+            
+          }
+        }
+ }
+  
+  while(term < 3){
+    pause();
+  }
 
   
   retrun 0;
