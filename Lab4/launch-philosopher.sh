191f7e196f6ef23b7c62db39e797969cd33d2875
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-05-07 22:13:43 -0700

    Update launch-philosopher.sh

--- Lab4/launch-philosopher.sh
+++ Lab4/launch-philosopher.sh
@@ -1,1 +1,15 @@
+#Ricardo Carrillo 
+#Lab4: RICE RICE AND RICE
+#05/07/2020
 
+program=./dining-p
+seats=$1
+position=$2
+
+if [ $# -gt 2 ];
+then
+echo "Error: input less arguments";
+exit 1;
+fi
+
+done
