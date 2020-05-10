b5be44e9f1af1de58ce584f5dbc0b55075174c11
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-05-09 17:25:17 -0700

    Update moles.c

--- Lab6/moles.c
+++ Lab6/moles.c
@@ -1,1 +1,18 @@
+//Ricardo Carrillo
+//05/09/2020
+//moles.c for Wack A Mole
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+    if (argc < 2) {
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *logfile = fopen(argv[2], "a+");
+	fprintf(logfile, "Pop %s\n", argv[1]);
+    fclose(logfile); 
+}
