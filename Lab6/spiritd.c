6e82b0fa3e637d8b78273d11ba7dae6b08de83d9
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-05-09 17:42:13 -0700

    Update spiritd.c

--- Lab6/spiritd.c
+++ Lab6/spiritd.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <fcntl.h>
+#include <string.h>
 
+void signalHandler(){
+  
+  
+  
+}
+
+
+int main(){
+  
+  
+  
+  
+  
+}
